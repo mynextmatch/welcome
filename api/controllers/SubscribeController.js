91548ba8fa9b41f62d7827862229cda679e819d9
Fabrizio <fabri_feno@yahoo.it>
2015-11-16 17:32:12 +0000

    ff

--- api/controllers/SubscribeController.js
+++ api/controllers/SubscribeController.js
@@ -5,6 +5,7 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+var Mandrill = require('machinepack-mandrill');
 
 module.exports = {
 
@@ -12,7 +13,28 @@ module.exports = {
 
     var email = req.body.email;
 
+
     Subscribe.create({email: email}).then(function(result){
+
+        Mandrill.sendPlaintextEmail({
+            apiKey: 'EXv6kVZPlOFoXyYm2',
+            toEmail: 'admin@mynextmatch.com',
+            toName: 'Mynextmatch Cooming soon',
+            subject: 'New subscriber for next releases',
+            message: 'a new subscriber with email: ' + result.email + ' want receive release information' ,
+            fromEmail: 'admin@mynextmatch.com',
+            fromName: 'Mynextmatch cooming soon',
+        }).exec({
+
+            error: function (err){
+
+            },
+
+            success: function (){
+
+            },
+        });
+
 
       res.json({status: "success", msg: "Thank you, we will get in touch."});
 
